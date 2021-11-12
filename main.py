d8f4aeb21f8eecfa818539a4366b4a07c2394918
Maurice Marquis <maravis05@gmail.com>
2021-11-12 12:45:34 -0500

    cleanup

--- main.py
+++ main.py
@@ -20,84 +20,84 @@ def check_dir(dir):
     
 
 
+if __name__ == "__main__":
+    while z != 'q':
+        config.deleted_files = 0
+        config.moved_files = 0
+        os.system('cls')
+        print("..........EYECUP FILE SCANNER..........\n")
+        choices = ['1 Create File Database','2 Take Backup from Box','3 Move Pixel Files','4 Move Duplicate Files, Create DB','5 Move Duplicate Files, Move Duplicate Timestamps, Create DB']
+        a = util.safe_input('What would you like to do?',choices)
 
-while z != 'q':
-    config.deleted_files = 0
-    config.moved_files = 0
-    os.system('cls')
-    print("..........EYECUP FILE SCANNER..........\n")
-    choices = ['1 Create File Database','2 Take Backup from Box','3 Move Pixel Files','4 Move Duplicate Files, Create DB','5 Move Duplicate Files, Move Duplicate Timestamps, Create DB']
-    a = util.safe_input('What would you like to do?',choices)
+        if a == '1':
+            config.td = (os.getenv("userprofile")+"\\Box\\Alderya EyeCup Images")
 
-    if a == '1':
-        config.td = (os.getenv("userprofile")+"\\Box\\Alderya EyeCup Images")
+            b = util.safe_input("Would you like to scan\n"+config.td,yesno)
+            if b == 'n': config.td = util.get_directory()
+            else: config.td = check_dir(config.td)
+            import create_database
+            create_database
 
-        b = util.safe_input("Would you like to scan\n"+config.td,yesno)
-        if b == 'n': config.td = util.get_directory()
-        else: config.td = check_dir(config.td)
-        import create_database
-        create_database
+        elif a == '2':
+            config.td = (os.getenv("userprofile")+"\\Box\\Alderya EyeCup Images")
+            config.dd = dd = "D:\\Aldeyra EyeCup Photos"
+            
+            b = util.safe_input("Would you like to backup\n"+config.td,yesno)
+            if b == 'n': config.td = util.get_directory()
+            else: config.td = check_dir(config.td)
+            b = util.safe_input("Backup to\n"+config.dd,yesno)
+            if b == 'n': config.dd = util.get_directory()
+            else: config.dd = check_dir(config.dd)
+            import take_backup
+            take_backup
 
-    elif a == '2':
-        config.td = (os.getenv("userprofile")+"\\Box\\Alderya EyeCup Images")
-        config.dd = dd = "D:\\Aldeyra EyeCup Photos"
+        elif a == '3':
+            config.td = (os.getenv("userprofile")+"\\Box\\Alderya EyeCup Images")
+            config.dd = "D:\\All Pixel Photos"
+
+            b = util.safe_input("Would you like to move Pixel files out of:\n"+config.td,yesno)
+            if b == 'n': config.td = util.get_directory()
+            else: config.td = check_dir(config.td)
+            b = util.safe_input("Move files to\n"+config.dd,yesno)
+            if b == 'n': config.dd = util.get_directory()
+            else: config.dd = check_dir(config.dd)
+            import move_pxl
+            move_pxl
+
+        elif a == '4':
+            config.td = (os.getenv("userprofile")+"\\Box\\Alderya EyeCup Images")
+            config.dd = dd = "D:\\Aldeyra EyeCup Photos"
+
+            b = util.safe_input("Would you like to scan\n"+config.td,yesno)
+            if b == 'n': config.td = util.get_directory()
+            else: config.td = check_dir(config.td)
+            b = util.safe_input("Move duplicates to\n"+config.dd,yesno)
+            if b == 'n': config.dd = util.get_directory()
+            else: config.dd = check_dir(config.dd)
+            config.move_dupe_file = 'yes'
+            import create_database
+            create_database
+
+        elif a == '5':
+            config.td = (os.getenv("userprofile")+"\\Box\\Alderya EyeCup Images")
+            config.dd = "D:\\Duplicate Timestamps"
+
+            b = util.safe_input("Would you like to scan\n"+config.td,yesno)
+            if b == 'n': config.td = util.get_directory()
+            else: config.td = check_dir(config.td)
+            b = util.safe_input("Move duplicates to\n"+config.dd,yesno)
+            if b == 'n': config.dd = util.get_directory()
+            else: config.dd = check_dir(config.dd)
+            config.find_dupe_time = 'yes'
+            import create_database
+            create_database
+            import move_dupe_time
+            move_dupe_time
         
-        b = util.safe_input("Would you like to backup\n"+config.td,yesno)
-        if b == 'n': config.td = util.get_directory()
-        else: config.td = check_dir(config.td)
-        b = util.safe_input("Backup to\n"+config.dd,yesno)
-        if b == 'n': config.dd = util.get_directory()
-        else: config.dd = check_dir(config.dd)
-        import take_backup
-        take_backup
+            
 
-    elif a == '3':
-        config.td = (os.getenv("userprofile")+"\\Box\\Alderya EyeCup Images")
-        config.dd = "D:\\All Pixel Photos"
-
-        b = util.safe_input("Would you like to move Pixel files out of:\n"+config.td,yesno)
-        if b == 'n': config.td = util.get_directory()
-        else: config.td = check_dir(config.td)
-        b = util.safe_input("Move files to\n"+config.dd,yesno)
-        if b == 'n': config.dd = util.get_directory()
-        else: config.dd = check_dir(config.dd)
-        import move_pxl
-        move_pxl
-
-    elif a == '4':
-        config.td = (os.getenv("userprofile")+"\\Box\\Alderya EyeCup Images")
-        config.dd = dd = "D:\\Aldeyra EyeCup Photos"
-
-        b = util.safe_input("Would you like to scan\n"+config.td,yesno)
-        if b == 'n': config.td = util.get_directory()
-        else: config.td = check_dir(config.td)
-        b = util.safe_input("Move duplicates to\n"+config.dd,yesno)
-        if b == 'n': config.dd = util.get_directory()
-        else: config.dd = check_dir(config.dd)
-        config.move_dupe_file = 'yes'
-        import create_database
-        create_database
-
-    elif a == '5':
-        config.td = (os.getenv("userprofile")+"\\Box\\Alderya EyeCup Images")
-        config.dd = "D:\\Duplicate Timestamps"
-
-        b = util.safe_input("Would you like to scan\n"+config.td,yesno)
-        if b == 'n': config.td = util.get_directory()
-        else: config.td = check_dir(config.td)
-        b = util.safe_input("Move duplicates to\n"+config.dd,yesno)
-        if b == 'n': config.dd = util.get_directory()
-        else: config.dd = check_dir(config.dd)
-        config.find_dupe_time = 'yes'
-        import create_database
-        create_database
-        import move_dupe_time
-        move_dupe_time
-    
-        
-
-    print("Request completed.\n")
-    choices = ['Quit','Restart Application']
-    z = util.safe_input("What would you like to do?",choices)
+        print("Request completed.\n")
+        choices = ['Quit','Restart Application']
+        z = util.safe_input("What would you like to do?",choices)
     
     
