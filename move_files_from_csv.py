9cf01b2afc1f8d9e9efa76a4340ba62a93677a59
Maurice Marquis <maravis05@gmail.com>
2021-11-19 18:33:46 -0500

    move from csv fix

--- move_files_from_csv.py
+++ move_files_from_csv.py
@@ -8,14 +8,16 @@ import os
 dest = (os.getenv("userprofile")+"\\Box\\Alderya EyeCup Images\\019 test")
 
 
+with open("c:\\temp\\database") as database:
+    database = csv.reader(database, delimiter=',')
 
 with open("c:\\temp\\move.csv") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
+    find_these = csv.reader(csv_file, delimiter=',')
     
-    for row in csv_reader:
-        for ea_file in all_good_files:
-            path = ea_file[9]+"\\"+ea_file[10]
-            ea_file.remove(ea_file[7:])
-            if row == ea_file:
+    for row in find_these:
+        for ea_file in database:
+            path = ea_file[10]+"\\"+ea_file[11]
+            new_file = ea_file[:7]
+            if row == new_file:
                 util.copy_this_file(path,dest)
             
